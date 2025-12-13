3512ad762dced83cb716a59bb9456f9a25b4c93a
Atle Magnussen <atlemagnussen@gmail.com>
2025-12-13 01:32:54 +0100

    ss

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddHttpLogging(options =>
 
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
-    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost;
     // options.KnownIPNetworks.Clear();
     // options.KnownProxies.Clear();
     // options.AllowedHosts.Clear();
