9172d0ebcebc7a8c5d11fc6af95078833ec8c75e
Atle Magnussen <atlemagnussen@gmail.com>
2025-12-13 10:25:37 +0100

    ch

--- rebuildstart.sh
+++ rebuildstart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-podman build -f Containerfile --tag yarp-oidc:latest .
+podman build -f Containerfile --tag atlmag/yarp-oidc:latest .
 
 #podman stop yarpOidc
 #podman run --name yarpOidc --env-file .env -d --rm -p 8002:8080 yarp-oidc .
